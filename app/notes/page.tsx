b5c41e0360b81cbaa55fcc5cd514c9f7d568b86e
Ait Hamid Adam <106628849+aithamid@users.noreply.github.com>
2023-12-12 14:41:36 +0100

    Env file

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,13 +1,19 @@
-import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers'
+'use client'
 
-export default async function Page() {
-  const cookieStore = cookies()
-  const supabase = createClient(cookieStore)
-  const { data: notes } = await supabase.from('notes').select()
-  const count2 = await (await supabase.from('notes').select('*', { count: 'exact' })).count
-  return <pre>
-    {JSON.stringify(count2, null, 2)}
-    {JSON.stringify(notes, null, 2)}
-    </pre>
+import { createClient } from '@/utils/supabase/client'
+import { useEffect, useState } from 'react'
+
+export default function Page() {
+  const [notes, setNotes] = useState<any[] | null>(null)
+  const supabase = createClient()
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await supabase.from('notes').select()
+      setNotes(data)
+    }
+    getData()
+  }, [supabase])
+
+  return <pre>{JSON.stringify(notes, null, 2)}</pre>
 }
