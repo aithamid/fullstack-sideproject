634752dd1a94ad8d5d95ce63b6c76fb02ba1531a
Ait Hamid Adam <106628849+aithamid@users.noreply.github.com>
2023-12-25 22:26:25 +0100

    Navbar progression

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,12 +3,23 @@ import { ModeToggle } from "@/components/ModeToggle"
 import { NavigationMenuDemo } from "@/components/NavBar"
 import { useTranslations } from "next-intl";
 import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
 
 export default function Home() {
   const t = useTranslations('Index');
   return (
+    <>
+    <NavigationMenuDemo />
     <div>
-      <ModeToggle /> <br />
       <Link href="/fr" locale="fr">
         Français
       </Link>
@@ -18,5 +29,7 @@ export default function Home() {
       </Link>
       <h1>{t('title')}</h1>
     </div>
+    
+    </>
   )
 }
